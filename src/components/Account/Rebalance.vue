2142c9c3896dd57b28143b1b7620fd9bd2f6b96d
Gurman Alexander <gurmanalexander@gmail.com>
2020-04-29 21:29:41 +0600

    pull out tab contents to components

---
+++ src/components/Account/Rebalance.vue
@@ -0,0 +1,110 @@
+<template>
+    <v-col>
+        <v-row class="d-block">
+            <div class="caption ml-3 mb-3 dark--text">From</div>
+            <v-select
+                placeholder="Choose exchange"
+                :items="items"
+                solo
+                background-color="light"
+                append-icon="mdi-chevron-down"
+                label="text"
+                flat
+            ></v-select>
+        </v-row>
+        <v-row class="d-block">
+            <div class="caption ml-3 mb-3 dark--text">To</div>
+            <v-select
+                placeholder="Choose exchange"
+                :items="items"
+                solo
+                background-color="light"
+                append-icon="mdi-chevron-down"
+                flat
+            ></v-select>
+        </v-row>
+        <v-row>
+            <v-select
+                placeholder="Choose currency"
+                :items="currencies"
+                solo
+                background-color="light"
+                append-icon="mdi-chevron-down"
+                flat
+            ></v-select>
+        </v-row>
+        <v-row>
+            <v-autocomplete
+                placeholder="Rebalance amount"
+                background-color="light"
+                append-icon="mdi-chevron-down"
+                solo
+                flat
+            >
+                <template v-slot:prepend-item>
+                    <v-list-item>
+                        <v-list-item-content>
+                            <div>
+                                <v-chip
+                                    class="caption"
+                                    color="rgba(42, 101, 145, 0.1)"
+                                    label
+                                    text-color="primary"
+                                >
+                                    Balance: <strong>0.04763864</strong>
+                                </v-chip>
+                            </div>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+                <template v-slot:no-data>
+                    <v-list-item>
+                        <v-list-item-content>
+                            <div>
+                                <v-btn
+                                    class="white--text mr-2 pa-2 text-capitalize"
+                                    v-for="percent in percents"
+                                    :key="percent"
+                                    small
+                                    depressed
+                                    color="label"
+                                >{{ percent }}
+                                </v-btn>
+                            </div>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-autocomplete>
+        </v-row>
+        <v-row>
+            <v-btn
+                class="text-capitalize"
+                color="primary"
+                block
+                x-large
+            >
+                Rebalance
+            </v-btn>
+        </v-row>
+    </v-col>
+</template>
+
+<script>
+    export default {
+        name: `Rebalance`,
+
+        components: {},
+
+        props: ['currencies', 'items'],
+
+        data: () => ({
+            percents: [
+                `10%`,
+                `25%`,
+                `50%`,
+                `100%`,
+                `split`
+            ]
+        }),
+    };
+</script>
---
+++ src/components/Account/Withdraw.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-col>
+        <v-row>
+            <v-select
+                placeholder="Choose exchange"
+                :items="items"
+                solo
+                background-color="light"
+                append-icon="mdi-chevron-down"
+                flat
+            ></v-select>
+        </v-row>
+        <v-row>
+            <v-select
+                placeholder="Choose currency"
+                :items="currencies"
+                solo
+                background-color="light"
+                append-icon="mdi-chevron-down"
+                flat
+            ></v-select>
+        </v-row>
+        <v-row>
+            <v-text-field
+                placeholder="Withdraw amount"
+                background-color="light"
+                solo
+                flat
+                v-model="sum"
+            >
+                <template v-slot:append>
+                    <v-btn
+                        class="text-capitalize"
+                        color="label"
+                        small
+                        dark
+                        depressed
+                        @click="sum = 1000"
+                    >
+                        max
+                    </v-btn>
+                </template>
+            </v-text-field>
+        </v-row>
+        <v-row>
+            <v-btn
+                class="text-capitalize"
+                color="primary"
+                block
+                x-large
+            >
+                Withdraw
+            </v-btn>
+        </v-row>
+    </v-col>
+</template>
+
+<script>
+    export default {
+        name: `Withdraw`,
+
+        components: {},
+
+        props: ['currencies', 'items'],
+
+        data() {
+            return {
+                sum: null
+            }
+        }
+    };
+</script>
